cc478e3264f6bc342d112ef52e74c6e74dbed832
xianyubo <xianyubo@bytedance.com>
2015-12-18 18:42:01 +0800

    add thrift

--- comment/src/main/resources/comment.thrift
+++ comment/src/main/resources/comment.thrift
@@ -1,12 +1,12 @@
-namespace py ss.comment.dedup
-namespace java ss.comment.dedup
+namespace py com.dedup.thrift.comment
+namespace java com.dedup.thrift.comment
 
-struct Comment {
+struct Req {
   1: i64 id,
   2: string content,
   3: i64 create_time,
 }
 
-service dedup {
-  map<i64, i64> dedup(1: list<Comment> comments)
+service Dedup {
+  map<i64, i64> dedup(1: list<Req> reqs)
 }
